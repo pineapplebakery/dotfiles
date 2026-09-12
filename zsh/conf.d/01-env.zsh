b563302e9539bf559309945eca90e5cb9635df39
pineapple.bakery <pineapplebakery.29@gmail.com>
2026-09-12 10:00:21 +0900

    update zsh

--- zsh/conf.d/01-env.zsh
+++ zsh/conf.d/01-env.zsh
@@ -23,3 +23,5 @@ if [ -f /etc/os-release ] && grep -qi '^ID=ubuntu' /etc/os-release; then
   export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 fi
+
+export PATH=$PATH:/usr/lib/wsl/lib
